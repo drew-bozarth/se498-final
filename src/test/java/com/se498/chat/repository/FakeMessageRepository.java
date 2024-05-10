465f19a0d3bdd71a417b5f4c07ffd7f57b585055
Drew-bozarth <dbozarth@chapman.edu>
2024-05-09 23:20:52 -0700

    updating messagerepository mock test

--- src/test/java/com/se498/chat/repository/FakeMessageRepository.java
+++ src/test/java/com/se498/chat/repository/FakeMessageRepository.java
@@ -6,60 +6,48 @@ import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class FakeMessageRepository{
 
     //TODO: Implement FakeMessageRepository
+    private final List<ChatMessage> messages;
 
-    private final Map<String, ChatMessage> db = new HashMap<>();
-
-
-    public void delete(ChatMessage arg0) {
+    public FakeMessageRepository() {
+        this.messages = new ArrayList<>();
+        messages.add(new ChatMessage("1", "John", "Hello World!", 10));
+        messages.add(new ChatMessage("2", "Jill", "Foobar", 20));
+        messages.add(new ChatMessage("3", "Joe", "words", 30));
+        messages.add(new ChatMessage("4", "Jane", "test message", 40));
+        messages.add(new ChatMessage("5", "Jake", "message test", 50));
     }
 
-    //@Override
-    public void deleteAllById(Iterable<? extends String> strings) {
+    public List<ChatMessage> findAll() {
+        return messages;
+    }
 
+    public int count() {
+        return messages.size();
+    }
+
+    public Optional<ChatMessage> findById(String id) {
+        return messages.stream()
+                .filter(message -> message.getMessageId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void save(ChatMessage message) {
+        messages.add(message);
+    }
+
+    public void delete(ChatMessage message) {
+        messages.remove(message);
     }
 
     public void deleteAll() {
-    }
-
-    public void deleteAll(Iterable<? extends ChatMessage> arg0) {
-    }
-
-    public void deleteById(String arg0) {
-    }
-
-    public boolean existsById(String arg0) {
-        return false;
-    }
-
-    public Iterable<ChatMessage> findAll() {
-        return this.db.values();
-    }
-
-    public Iterable<ChatMessage> findAllById(Iterable<String> arg0) {
-
-        return null;
-    }
-
-    //@Override
-    public long count() {
-        return 0;
-    }
-
-    public Optional<ChatMessage> findById(String arg0) {
-        return Optional.of(this.db.get(arg0));
-    }
-
-    public <S extends ChatMessage> S save(S arg0) {
-        this.db.put(arg0.getUsername(), arg0);
-        return arg0;
-    }
-
-    public <S extends ChatMessage> Iterable<S> saveAll(Iterable<S> arg0) {
-        return null;
+        messages.clear();
     }
 }
--- src/test/java/com/se498/chat/repository/MessageRepositoryMockTest.java
+++ src/test/java/com/se498/chat/repository/MessageRepositoryMockTest.java
@@ -1,35 +1,52 @@
 package com.se498.chat.repository;
 
 import com.se498.chat.TestChatApplication;
+import java.util.List;
 import com.se498.chat.model.ChatMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = {TestChatApplication.class})
 public class MessageRepositoryMockTest {
     //TODO: Implement message repository Mock test
 
-    @Autowired
-    //@Qualifier("fakeMessageRepository")
-    private MessageRepository messageRepository;
+    @InjectMocks
+    private FakeMessageRepository messageRepository;
 
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
-    //@Disabled // Disabling my written tests for now
     @Test
-    public void testMessageRepository(){
-
-        ChatMessage dummyMessage = new ChatMessage();
-        dummyMessage.setUsername("John");
-        dummyMessage.setMessageText("Hello World!");
-        dummyMessage.setSeed(10);
-        dummyMessage.setMessageId("1");
-        //ChatMessage dummyMessage = new ChatMessage("1", "John", "Hello World!", 10);
-        messageRepository.save(dummyMessage);
-        assertEquals (dummyMessage, messageRepository.findByID("1").get());
+    public void testFindAll() {
+        List<ChatMessage> messages = messageRepository.findAll();
+        assertEquals(5, messageRepository.count());
+        // message 1
+        assertEquals("1", messages.get(0).getMessageId());
+        assertEquals("Hello World!", messages.get(0).getMessageText());
+        assertEquals(10, messages.get(0).getSeed());
+        // message 2
+        assertEquals("2", messages.get(0).getMessageId());
+        assertEquals("Foobar", messages.get(0).getMessageText());
+        assertEquals(20, messages.get(0).getSeed());
+        // message 3
+        assertEquals("3", messages.get(0).getMessageId());
+        assertEquals("words", messages.get(0).getMessageText());
+        assertEquals(30, messages.get(0).getSeed());
+        // message 4
+        assertEquals("4", messages.get(0).getMessageId());
+        assertEquals("test message", messages.get(0).getMessageText());
+        assertEquals(40, messages.get(0).getSeed());
+        // message 5
+        assertEquals("5", messages.get(0).getMessageId());
+        assertEquals("message test", messages.get(0).getMessageText());
+        assertEquals(50, messages.get(0).getSeed());
     }
 }
